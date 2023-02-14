bd2f7bef2cafd414b4822fd85a06e29775554834
dong.ho <hophuongdong25052001@gmail.com>
2023-02-14 18:07:36 +0700

    first commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void HandlePlayButtonOnClickEvent()
+    {
+        AudioManager.Play(AudioClipName.PlayClick);
+        MenuManager.GoToMenu(MenuName.Pause);
+    }
+
+    ///// <summary>
+    ///// Handles the on click event from the high score button
+    ///// </summary>
+    //public void HandleHighScoreButtonOnClickEvent()
+    //{
+    //    AudioManager.Play(AudioClipName.MenuButtonClick);
+    //    MenuManager.GoToMenu(MenuName.HighScore);
+    //}
+
+    /// <summary>
+    /// Handles the on click event from the quit button
+    /// </summary>
+    public void HandleQuitButtonOnClickEvent()
+    {
+        AudioManager.Play(AudioClipName.ExitClick);
+        Application.Quit();
+    }
+}
