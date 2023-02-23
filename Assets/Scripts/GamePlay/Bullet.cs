2ad295b344493413915bdd5009b278d3b529f15f
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2023-02-23 09:36:24 +0700

    sửa Player2, thêm đạn

--- Assets/Scripts/GamePlay/Bullet.cs
+++ Assets/Scripts/GamePlay/Bullet.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+
 public class Bullet : MonoBehaviour
 {
     private float speed = 5f;
-
-    //private Vector2 dir;
+  
+    private Vector3 dir;
+    private Vector3 firePoint;
 
     void Start()
     {
-        //dir = GameObject.Find("Dir").transform.position;
+        dir = GameObject.Find("Dir").transform.position;
+        firePoint = GameObject.Find("FirePoint").transform.position;
         transform.position = GameObject.Find("FirePoint").transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Vector2.MoveTowards(transform.position, dir, speed* Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, dir, speed * Time.deltaTime);
+        if(transform.position.x - firePoint.x > 30 || transform.position.y - firePoint.y > 30)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
--- Assets/Scripts/GamePlay/CharacterController.cs
+++ Assets/Scripts/GamePlay/CharacterController.cs
@@ -14,9 +14,11 @@ public class CharacterController : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
-    public AudioSource shootAudio;
+    public AudioClip shootAudio;
     int vector = 1;
     public Vector3 offset = new Vector3(0, 0.1f, 0);
+    float horizontal;
+    private int dir = 0;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -29,7 +31,7 @@ public class CharacterController : MonoBehaviour
     {
         inputVector2.x = Input.GetAxisRaw("Horizontal");
         inputVector2.y = Input.GetAxisRaw("Vertical");
-
+        horizontal = Input.GetAxisRaw("Horizontal");
         // shoot as appropriate
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -37,8 +39,19 @@ public class CharacterController : MonoBehaviour
             bullet.transform.eulerAngles = new Vector3(0, 0, -90);
             Rigidbody2D rg = bullet.GetComponent<Rigidbody2D>();
             rg.AddForce(weapon1.right * vector * 1000f);
-            shootAudio.Play();
+            shootAudio.LoadAudioData();
             //shootSoundEffect.Play();
+        }
+
+        if (horizontal > 0 && dir == 1)
+        {
+            dir = 0;
+            transform.Rotate(0, 180, 0);
+        }
+        if (horizontal < 0 && dir == 0)
+        {
+            dir = 1;
+            transform.Rotate(0, 180, 0);
         }
     }
 
@@ -50,14 +63,14 @@ public class CharacterController : MonoBehaviour
     private void LateUpdate()
     {
         animator.SetFloat("Speed", inputVector2.magnitude);
-        if (inputVector2.x != 0)
-        {
-            GameObject weapon = GameObject.FindGameObjectWithTag("Weapon");
-            SpriteRenderer sprite = weapon.GetComponent<SpriteRenderer>();
-            spriteRenderer.flipX = inputVector2.x < 0;
-            sprite.flipX = inputVector2.x < 0;
-            vector = inputVector2.x < 0 ? -1 : 1;
-        }
+        //if (inputVector2.x != 0)
+        //{
+        //    GameObject weapon = GameObject.FindGameObjectWithTag("Weapon");
+        //    SpriteRenderer sprite = weapon.GetComponent<SpriteRenderer>();
+        //    spriteRenderer.flipX = inputVector2.x < 0;
+        //    sprite.flipX = inputVector2.x < 0;
+        //    vector = inputVector2.x < 0 ? -1 : 1;
+        //}
     }
 
 
