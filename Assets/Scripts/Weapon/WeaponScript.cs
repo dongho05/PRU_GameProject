5ed65ab672bb3175c8cd580a03dc500468b9d454
dong.ho <hophuongdong25052001@gmail.com>
2023-03-07 14:24:59 +0700

    add box weapon, pickup box and change weapon

---
+++ Assets/Scripts/Weapon/WeaponScript.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    int totalWeapons = 1;
+    public int currentWeaponIndex;
+    public GameObject[] guns;
+    public GameObject weaponHolder;
+    public GameObject currentGun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalWeapons = weaponHolder.transform.childCount;
+        guns = new GameObject[totalWeapons];
+        for (int i = 0; i < totalWeapons; i++)
+        {
+            guns[i] = weaponHolder.transform.GetChild(i).gameObject;
+            guns[i].SetActive(false);
+        }
+        guns[0].SetActive(true);
+        currentGun = guns[0];
+        currentWeaponIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("M4"))
+        {
+            if (currentWeaponIndex == 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 2)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex -= 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 3)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex -= 2;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+        }
+        if (collision.gameObject.CompareTag("Ak"))
+        {
+            if (currentWeaponIndex != 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex = 0;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+
+        }
+        if (collision.gameObject.CompareTag("Pistol"))
+        {
+            if (currentWeaponIndex == 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 2;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 1)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 3)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex -= 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+        }
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+            if (currentWeaponIndex == 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 3;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 1)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 2;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            else if (currentWeaponIndex == 2)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+        }
+    }
+}
